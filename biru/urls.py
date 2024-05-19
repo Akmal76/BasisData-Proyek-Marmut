5a6a8ea76666b5aff49c91c5b4b5503fe8a6fa16
sunflawlxs <sherylivana99@gmail.com>
2024-05-19 13:29:47 +0700

    crud podcast uda, ru chart udah, sisa play podcast

--- biru/urls.py
+++ biru/urls.py
@@ -5,7 +5,7 @@ from . import views
 
 urlpatterns = [
     path('podcast/<uuid:podcast_id>/', podcast, name='podcast'),
-    path('podcasts/<int:podcast_id>/', get_podcast_details, name='get_podcast_details'),
+    path('podcast-detail/<uuid:podcast_id>/', views.get_podcast_details, name='get_podcast_details'),
     path('chartDetails/', views.get_chart_details, name='get_chart_details'),
     path('chartDetailsWeekly/', views.get_chart_details_weekly, name='get_chart_details_weekly'),
     path('chart/', chart_view, name='chart'),
@@ -13,6 +13,6 @@ urlpatterns = [
     path('delete_podcast/<uuid:podcast_id>/', delete_podcast, name='delete_podcast'),
     path('list_podcasts/', podcast, name='podcast'),
     path('create_episode/<uuid:podcast_id>/', create_episode, name='create_episode'),
-    path('delete_episode/<uuid:episode_id>/', delete_episode, name='delete_episode'),
-    path('list_episodes/<str:podcast_id>/', views.list_episode, name='list_episodes'),
+    path('delete_episode/<uuid:episode_id>/<uuid:podcast_id>/', delete_episode, name='delete_episode'),
+    path('episodes/<str:podcast_id>/', views.list_episode, name='episodes'),
 ]
--- biru/views.py
+++ biru/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.db import connection
 import uuid
@@ -9,22 +10,21 @@ def get_podcast_details(request, podcast_id):
         # Fetch podcast details
         cursor.execute("""
             SELECT K.judul, array_agg(G.genre), A.nama, K.durasi, K.tanggal_rilis, K.tahun
-            FROM marmut.KONTEN K
-            JOIN marmut.PODCAST P ON K.id = P.id_konten
-            JOIN marmut.PODCASTER Po ON P.email_podcaster = Po.email
-            JOIN marmut.AKUN A ON Po.email = A.email
-            JOIN marmut.GENRE G ON K.id = G.id_konten
+            FROM KONTEN K
+            JOIN PODCAST P ON K.id = P.id_konten
+            JOIN PODCASTER Po ON P.email_podcaster = Po.email
+            JOIN AKUN A ON Po.email = A.email
+            JOIN GENRE G ON K.id = G.id_konten
             WHERE K.id = %s
             GROUP BY K.judul, A.nama, K.durasi, K.tanggal_rilis, K.tahun
-        """, [str(podcast_id)])
+    """, [str(podcast_id)])
 
         podcast = cursor.fetchone()
-        print(podcast)
+        print('ayam', podcast)
 
-        # Fetch podcast episodes
         cursor.execute("""
             SELECT E.judul, E.deskripsi, E.durasi, E.tanggal_rilis
-            FROM marmut.EPISODE E
+            FROM EPISODE E
             WHERE E.id_konten_podcast = %s
             ORDER BY E.tanggal_rilis DESC
         """, [str(podcast_id)])
@@ -39,12 +39,10 @@ from django.db import connection
 
 def get_chart_details(request):
     with connection.cursor() as cursor:
-        # Fetch chart types
         cursor.execute("SELECT DISTINCT tipe FROM Chart")
         chart_types = cursor.fetchall()
         print("Chart Types:", chart_types)
 
-        # Fetch chart details for 'Daily Top 20' only
         chart_details = {}
         for chart_type in chart_types:
             if chart_type[0] == 'Daily Top 20':
@@ -66,15 +64,19 @@ def get_chart_details(request):
     return render(request, 'chartdetail.html', {'chart_details': chart_details, 'chart_types': chart_types})
 
 def create_episode(request, podcast_id):
+    with connection.cursor() as cursor:
+        cursor.execute("""
+            SELECT K.judul
+            FROM KONTEN K
+            WHERE K.id = %s
+        """, [podcast_id])
+        podcast_title = cursor.fetchone()[0]  
+        
     if request.method == 'POST':
         title = request.POST['title']
         description = request.POST['description']
-        duration = request.POST['duration']
-        
-        # Generate UUID for id_episode
+        duration = request.POST['duration']  
         id_episode = uuid.uuid4()
-        
-        # Get current timestamp
         current_time = timezone.now()
         with connection.cursor() as cursor:
             cursor.execute("""
@@ -82,49 +84,73 @@ def create_episode(request, podcast_id):
                 VALUES (%s, %s, %s, %s, %s, %s)
             """, [id_episode, podcast_id, title, description, duration, current_time])
 
-        return redirect('list_episodes', podcast_id=podcast_id)
-
-    return render(request, 'createepisode.html')
+        return redirect('episodes', podcast_id=podcast_id)
+        
+    return render(request, 'createepisode.html', {'podcast_id': podcast_id, 'podcast_title': podcast_title})
 
 def create_podcast(request):
     if request.method == 'POST':
-        title = request.POST['title']
+        title = request.POST.get('title')
         genre = request.POST.getlist('genre')
-        duration = request.POST['duration']
+        duration = 0
+        podcast_id = uuid.uuid4()
+        current_time = timezone.now()
 
         with connection.cursor() as cursor:
             cursor.execute("""
-                INSERT INTO marmut.KONTEN (judul, durasi)
-                VALUES (%s, %s)
-                RETURNING id
-            """, [title, duration])
-            podcast_id = cursor.fetchone()[0]
+                INSERT INTO KONTEN (id, judul, tanggal_rilis, tahun, durasi)
+                VALUES (%s, %s, %s, %s, %s)
+            """, [podcast_id, title, current_time, current_time.year, duration])
 
             for g in genre:
                 cursor.execute("""
-                    INSERT INTO marmut.GENRE (id_konten, genre)
+                    INSERT INTO GENRE (id_konten, genre)
                     VALUES (%s, %s)
                 """, [podcast_id, g])
 
-        return redirect('list_podcasts')
+            cursor.execute("""
+                INSERT INTO PODCAST (id_konten)
+                VALUES (%s)
+            """, [podcast_id])
+        
+        return redirect('podcast')  
 
     return render(request, 'createpodcast.html')
 
+from django.db import IntegrityError
+
 def delete_podcast(request, podcast_id):
     if request.method == 'POST':
-        with connection.cursor() as cursor:
-            cursor.execute("""
-                DELETE FROM marmut.KONTEN
-                WHERE id = %s
-            """, [podcast_id])
+        try:
+            with connection.cursor() as cursor:
+                cursor.execute("""
+                    DELETE FROM PODCAST
+                    WHERE id_konten = %s
+                """, [podcast_id])
+                
+                cursor.execute("""
+                    DELETE FROM GENRE
+                    WHERE id_konten = %s
+                """, [podcast_id])
+                
+                cursor.execute("""
+                    DELETE FROM KONTEN
+                    WHERE id = %s
+                """, [podcast_id])
+        except IntegrityError as e:
+            # Handle integrity error if needed
+            print("IntegrityError:", e)
+            pass
 
-        return redirect('list_podcasts')
+        return redirect('podcast')
+
 
 def podcast(request):
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT K.id, K.judul, COUNT(E.id_konten_podcast), SUM(E.durasi)
+            SELECT K.id, K.judul, COUNT(E.id_episode) AS jumlah_episode, COALESCE(SUM(E.durasi), 0) AS total_durasi
             FROM KONTEN K
+            JOIN PODCAST P ON K.id = P.id_konten
             LEFT JOIN EPISODE E ON K.id = E.id_konten_podcast
             GROUP BY K.id, K.judul;
         """)
@@ -132,30 +158,27 @@ def podcast(request):
         print(podcasts)
     return render(request, 'podcast.html', {'podcasts': podcasts})
 
-
-
-
-def delete_episode(request, episode_id):
+def delete_episode(request, episode_id, podcast_id):
     if request.method == 'POST':
         with connection.cursor() as cursor:
             cursor.execute("""
-                DELETE FROM marmut.EPISODE
-                WHERE id = %s
+                DELETE FROM EPISODE
+                WHERE id_episode = %s
             """, [episode_id])
 
-        return redirect('list_episodes')
+        return redirect('episodes', podcast_id=podcast_id)
 
 def list_episode(request, podcast_id):
     with connection.cursor() as cursor:
         cursor.execute("""
-            SELECT E.judul, E.deskripsi, E.durasi, E.tanggal_rilis
+            SELECT E.id_episode, E.judul, E.deskripsi, E.durasi, E.tanggal_rilis
             FROM EPISODE E
             WHERE E.id_konten_podcast = %s
             ORDER BY E.tanggal_rilis DESC
         """, [podcast_id])
         episode = cursor.fetchall()
         print(episode)
-    return render(request, 'episode.html', {'episode': episode})
+    return render(request, 'episode.html', {'episode': episode, 'podcast_id': podcast_id})
 
 def show_dashboard(request):
     # Your view logic here
